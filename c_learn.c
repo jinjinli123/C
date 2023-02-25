7aaf20c59e1679b6884d595219d7cfb22420f32c
lijin <yuju62649@163.com>
2023-02-25 20:16:10 +0800

    第一次提交

---
+++ c_learn.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+   
+
+    // printf("int = %d\n",sizeof(int));
+    // printf("short int = %d\n",sizeof(short));
+    // printf("long int = %d\n",sizeof(long));
+    // printf("long long int = %d\n",sizeof(long long));
+    // printf("char = %d\n",sizeof(char));
+    // //printf("_Bool = %d\n",sizeof(_Bool));
+    // printf("float = %d\n",sizeof(float));
+    // printf("double = %d\n",sizeof(double));
+    // printf("long double = %d\n",sizeof(long double));
+
+    short i;
+    unsigned short j;
+    
+    i = -1;
+    j = -1;
+
+    printf("%d\n",i);//d是带符号的
+    printf("%u\n",j);//u是不带符号的
+
+
+
+
+    return 0;
+
+} 

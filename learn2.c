15505ba494c328672c4226b3c69259f15f26988b
lijin <yuju62649@163.com>
2023-02-26 11:27:04 +0800

    字符串

--- learn2.c
+++ learn2.c
@@ -3,9 +3,23 @@
 
 int main()
 {
-    char a = 67;
-    
+    char a = 'C';
+
     printf("c = %d\n",a);
 
     return 0;
 }
+	// 1. char
+	// char是一个特殊的整形
+	
+	// char是否unsigned由编译系统决定
+	
+    // 2. 字符串
+
+	// 声明: char 变量名[数量];
+	
+	// 赋值: 变量名[索引号] = 字符
+	// 例：  声明： char name[2];
+	// 	赋值：name[0] = 'L';
+	// 			name[1] = 'J'；
+	// 简写：char  变量名[] = 字符串常量
